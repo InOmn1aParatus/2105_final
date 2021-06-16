8e0431952edbdb29df259149874505e7ca432cde
Matt Kragen <mattdkragen@gmail.com>
2021-06-16 08:59:23 -0700

    Complete weight method

--- lib/train.rb
+++ lib/train.rb
@@ -1,10 +1,11 @@
 class Train
-  attr_reader :name, :type, :cargo
+  attr_reader :name, :type, :cargo, :weight
 
   def initialize(info)
     @name = info[:name]
     @type = info[:type]
     @cargo = Hash.new(0)
+    @weight = 0
   end
 
   def count_cars(car)
@@ -13,5 +14,12 @@ class Train
 
   def add_cars(car, quantity)
     cargo[car] += quantity
+  end
+
+  def weight
+    @cargo.each do |car, quantity|
+      @weight += car.weight * quantity
+    end
+    @weight
   end
 end
