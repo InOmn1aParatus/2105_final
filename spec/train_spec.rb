b69f20d52b2e0312e6d1aec044478bff38084c8c
Matt Kragen <mattdkragen@gmail.com>
2021-06-16 08:13:27 -0700

    Add attribute test

--- spec/train_spec.rb
+++ spec/train_spec.rb
@@ -9,6 +9,13 @@ RSpec.describe '' do
       train1 = Train.new({name: 'Thomas', type: 'Tank'})
       expect(train1).to be_a(Train)
     end
+
+    it 'has attributes and starts with no cargo' do
+      train1 = Train.new({name: 'Thomas', type: 'Tank'})
+      expect(train1.name).to eq('Thomas')
+      expect(train1.type).to eq('Tank')
+      expect(train1.cargo).to eq({})
+    end
   end
 
   describe 'methods' do
