c44332d5da0e1019a80ddc140525139fae3afedf
Matt Kragen <mattdkragen@gmail.com>
2021-06-16 08:34:34 -0700

    Create class with #init, attr reader, add trains method

---
+++ lib/train_yard.rb
@@ -0,0 +1,12 @@
+class TrainYard
+  attr_reader :location, :trains
+
+  def initialize(info)
+    @location = info[:location]
+    @trains = []
+  end
+
+  def add_train(train)
+    @trains << train
+  end
+end
