a65d6261391900211741625a4554ff7a87721801
Matt Kragen <mattdkragen@gmail.com>
2021-06-16 08:22:05 -0700

    Write out test for adding cars

--- spec/train_spec.rb
+++ spec/train_spec.rb
@@ -19,5 +19,22 @@ RSpec.describe '' do
   end
 
   describe 'methods' do
+    it 'counts cars given car object' do
+      car1 = Car.new({type: 'Mail', weight: 5})
+      car2 = Car.new({type: 'Passengers', weight: 1})
+      train1 = Train.new({name: 'Thomas', type: 'Tank'})
+      expect(train1.count_cars(car1)).to eq(0)
+
+      train1.add_cars(car1, 2)
+      expect(train1.cargo).to eq({car1 => 2})
+      expect(train1.count_cars(car1)).to eq(2)
+
+      train1.add_cars(car1, 3)
+      expect(train1.cargo).to eq({car1 => 5})
+      expect(train1.count_cars(car1)).to eq(5)
+
+      train1.add_cars(car2, 10)
+      expect(train1.cargo).to eq({car1 => 5, car2 => 10})
+    end
   end
 end
